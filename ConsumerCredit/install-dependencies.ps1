f34a05e94a98ba2d49bae11ee4b151ea41c0bfb9
Damien <damien.hanoun@younited-credit.fr>
2024-10-30 12:25:44 +0100

    Simplify dependencies installation file

--- ConsumerCredit/install-dependencies.ps1
+++ ConsumerCredit/install-dependencies.ps1
@@ -34,63 +34,71 @@ function Check-Winget {
     return $wingetInstalled
 }
 
-# Function to install winget (if needed) and ensure its path is added
+# Check if winget is installed
 function Install-Winget {
-    Write-Host "Attempting to install winget..."
+    if (-not (Check-Winget))
+    {
+        Write-Host "winget is not installed. Attempting installation via GitHub..."
+        Write-Host "Attempting to install winget..."
 
-    # Define download URL and file path
-    $wingetDownloadUrl = "https://github.com/microsoft/winget-cli/releases/download/v1.8.1911/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
-    $destinationPath = "$env:USERPROFILE\Downloads\winget.msixbundle"
+        # Define download URL and file path
+        $wingetDownloadUrl = "https://github.com/microsoft/winget-cli/releases/download/v1.8.1911/Microsoft.DesktopAppInstaller_8wekyb3d8bbwe.msixbundle"
+        $destinationPath = "$env:USERPROFILE\Downloads\winget.msixbundle"
 
-    # Check if the file already exists
-    if (Test-Path $destinationPath) {
-        Write-Host "Winget msixbundle already exists at $destinationPath. Skipping download."
-    } else {
-        # Attempt to download the file
-        try {
-            Write-Host "Downloading Winget from GitHub..."
-            Invoke-WebRequest -Uri $wingetDownloadUrl -OutFile $destinationPath -UseBasicParsing
-            Write-Host "Download complete. File saved to $destinationPath"
-        } catch {
-            Write-Host "Failed to download Winget. Please check your internet connection or try downloading the file manually."
+        # Check if the file already exists
+        if (Test-Path $destinationPath)
+        {
+            Write-Host "Winget msixbundle already exists at $destinationPath. Skipping download."
+        }
+        else
+        {
+            # Attempt to download the file
+            try
+            {
+                Write-Host "Downloading Winget from GitHub..."
+                Invoke-WebRequest -Uri $wingetDownloadUrl -OutFile $destinationPath -UseBasicParsing
+                Write-Host "Download complete. File saved to $destinationPath"
+            }
+            catch
+            {
+                Write-Host "Failed to download Winget. Please check your internet connection or try downloading the file manually."
+                return
+            }
+        }
+
+        # Install the MSIX bundle using Add-AppxPackage
+        try
+        {
+            Write-Host "Installing Winget using Add-AppxPackage..."
+            Add-AppxPackage -Path $destinationPath
+            Write-Host "Installation completed."
+        }
+        catch
+        {
+            Write-Host "Failed to install winget. Please install manually."
             return
         }
+
+        # After installation, search for winget.exe and add to PATH
+        Write-Host "Searching for winget.exe after installation..."
+        $wingetPath = Get-ChildItem -Path 'C:\Program Files\WindowsApps' -Recurse -Filter winget.exe -ErrorAction SilentlyContinue | Select-Object -First 1
+
+        if ($wingetPath)
+        {
+            Write-Host "winget found at $( $wingetPath.DirectoryName ). Adding to PATH."
+            $env:Path += ";$( $wingetPath.DirectoryName )"
+        }
+        else
+        {
+            Write-Host "winget.exe not found after installation."
+        }
     }
-
-    # Install the MSIX bundle using Add-AppxPackage
-    try {
-        Write-Host "Installing Winget using Add-AppxPackage..."
-        Add-AppxPackage -Path $destinationPath
-        Write-Host "Installation completed."
-    } catch {
-        Write-Host "Failed to install winget. Please install manually."
-        return
-    }
-
-    # After installation, search for winget.exe and add to PATH
-    Write-Host "Searching for winget.exe after installation..."
-    $wingetPath = Get-ChildItem -Path 'C:\Program Files\WindowsApps' -Recurse -Filter winget.exe -ErrorAction SilentlyContinue | Select-Object -First 1
-
-    if ($wingetPath) {
-        Write-Host "winget found at $($wingetPath.DirectoryName). Adding to PATH."
-        $env:Path += ";$($wingetPath.DirectoryName)"
-    } else {
-        Write-Host "winget.exe not found after installation."
+    else
+    {
+        Write-Host "winget is already installed."
     }
 }
 
-# Check if winget is installed
-if (-not (Check-Winget)) {
-    Write-Host "winget is not installed. Attempting installation via GitHub..."
-    Install-Winget
-} else {
-    Write-Host "winget is already installed."
-}
-
-
-
-
-# Function to check if a specific .NET SDK version is installed
 function Check-Net8 {
     $net8Installed = $false
     try {
@@ -142,90 +150,85 @@ function Install-DockerDesktop {
     }
 }
 
-# Function to add certificate to Trusted Root Certification Authorities
-function Generate-And-Trust-Certificates {    
-    # Define the base directory (where this script is located)
-	$baseDir = $PSScriptRoot
-	$serverProjectPath = Join-Path $baseDir "Acquisition.WebApplication\Acquisition.WebApplication.Server"
+function Install-NSwag {
+    Write-Host "Installing nswag"
+    dotnet tool install --global NSwag.ConsoleCore
+}
 
-	# Set developper certificate for local api
+# Check if Chocolatey is installed
+function Install-Chocolatey {
+    if (!(Get-Command choco -ErrorAction SilentlyContinue))
+    {
+        Write-Host "Chocolatey not found. Installing Chocolatey..."
+
+        # Download and install Chocolatey
+        Set-ExecutionPolicy Bypass -Scope Process -Force
+        [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072
+        Invoke-Expression ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+
+        Write-Host "Chocolatey installed successfully."
+    }
+    else
+    {
+        Write-Host "Chocolatey is already installed."
+    }
+}
+
+# Function to add certificate to Trusted Root Certification Authorities
+function Generate-And-Trust-Certificates {
+	$baseDir = $PSScriptRoot
+
+	# Step 1: Set developper certificate for local api
+    Write-Host "Generate API certificate"    
 	dotnet dev-certs https --clean
 	dotnet dev-certs https --trust
 
-	# Step 1: Navigate to the Angular client project folder and install dependencies
-	$clientProjectPath = Join-Path $baseDir "Acquisition.WebApplication\acquisition.webapplication.client"
+    # Step 2: Generate certificate for angular
+    Write-Host "Generate Angular certificate"
+    $clientProjectPath = Join-Path $baseDir "Acquisition.WebApplication\acquisition.webapplication.angular.client\ssl"
+    
+    mkcert -install
+    Set-Location $clientProjectPath
+    mkcert localhost
+    
+    Set-Location $baseDir
+}
 
-	if (Test-Path "$clientProjectPath\package.json") {
-		Write-Host "Navigating to the Angular project folder..."
-		Set-Location $clientProjectPath
+function Install-MkCert {
+    Write-Host "Installing MkCert"
+    choco install mkcert -y
+}
 
-		Write-Host "Installing necessary dependencies..."
-		npm install
-		Write-Host "Dependencies installed."
-	} else {
-		Write-Host "package.json not found in the Angular project directory."
-		exit 1
-	}
+function Npm-Install {
+    $baseDir = $PSScriptRoot
+    $clientProjectPath = Join-Path $baseDir "Acquisition.WebApplication\acquisition.webapplication.angular.client"
+    
+    if (Test-Path "$clientProjectPath\package.json") {
+        Write-Host "Navigating to the Angular project folder..."
+        Set-Location $clientProjectPath
 
-	# Step 2: Navigate to the server project and start the server, which also starts Angular
-	if (Test-Path "$serverProjectPath\Acquisition.WebApplication.Server.csproj") {
-		Write-Host "Navigating to the Server project folder..."
-		Set-Location $serverProjectPath
-
-		Write-Host "Starting Acquisition.WebApplication.Server project..."
-		
-		# Start the server project (which will also start Angular)
-		# Assumes you're using .NET SDK, adjust the command if using different tooling.
-		$serverProcess = Start-Process "dotnet" -ArgumentList "run" -PassThru -NoNewWindow -ErrorAction Stop
-
-		# Give the server some time to start and Angular to generate the certificate (e.g., 10-15 seconds)
-		Start-Sleep -Seconds 15
-
-		# Step 3: Stop the server process if needed (optional)
-		if ($serverProcess.HasExited) {
-			Write-Host "Server process has already exited."
-		} else {
-			Write-Host "Stopping the server..."
-			Stop-Process -Id $serverProcess.Id
-		}
-	} else {
-		Write-Host "Server project not found in the directory."
-		exit 1
-	}
-
-	# Step 4: Add the generated certificate to Trusted Root Certification Authorities
-	$certPath = "$env:APPDATA\ASP.NET\https\acquisition.webapplication.client.pem"
-	if (Test-Path $certPath) {
-		Write-Host "Adding SSL certificate to Trusted Root Certification Authorities..."
-		certutil -addstore "Root" $certPath
-		Write-Host "Certificate added successfully."
-	} else {
-		Write-Host "Certificate not found at $certPath. Make sure the certificate is generated."
-	}
-	
-	Set-Location $baseDir
+        Write-Host "Installing necessary dependencies..."
+        npm install
+        Write-Host "Dependencies installed."
+    } else {
+        Write-Host "package.json not found in the Angular project directory."
+        exit 1
+    }
+    
+    Set-Location $baseDir
 }
 
 # Main script logic
-Write-Host "Starting installation check for .NET 8 SDK and Docker Desktop..."
+Write-Host "Starting installation of dependencies..."
 
-# Ensure winget is available
-if (-not (Get-Command winget -ErrorAction SilentlyContinue)) {
-    Write-Host "winget is not available. Please install winget manually."
-    exit 1
-}
-
-# Check and install .NET 8 SDK
+# Install dependencies
+Install-Chocolatey
+Install-MkCert
+Install-Winget
+Install-NSwag
 Install-Net8
-
-# Check and install Docker Desktop
 Install-DockerDesktop
-
-# Install nswag before trying to launch the project that require it
-Write-Host "Installing nswag"
-dotnet tool install --global NSwag.ConsoleCore
-
-# Trust the certificate if it exists
+Npm-Install
 Generate-And-Trust-Certificates
 
 Write-Host "All checks complete."
